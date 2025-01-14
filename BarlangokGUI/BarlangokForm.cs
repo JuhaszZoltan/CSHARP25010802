a3268102e8a0211f1a08fbbfab70adf091c82a61
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-01-14 12:16:45 +0100

    ads

--- BarlangokGUI/BarlangokForm.cs
+++ BarlangokGUI/BarlangokForm.cs
@@ -4,10 +4,11 @@ namespace BarlangokGUI
 {
     public partial class BarlangokForm : Form
     {
-        private const string SOLDIR =
-    "C:\\Users\\juhaszz\\Source\\Repos\\CSHARP25010802\\SRC";
+        private const string SOLDIR = "C:\\Users\\juhaszz\\Source\\Repos\\CSHARP25010802\\SRC";
 
         List<Barlang> barlangok = [];
+        Barlang kijBarlang = null;
+
         public BarlangokForm()
         {
             InitializeComponent();
@@ -17,7 +18,20 @@ namespace BarlangokGUI
 
         private void BtnKeresesClick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            kijBarlang = barlangok
+                .SingleOrDefault(b => b.Azon == int.Parse(tbAzonosito.Text));
+
+            if (kijBarlang is null)
+            {
+                _ = MessageBox.Show("Ezzel az azonosítóval nem létezik barlang!");
+                EredetiAllapot();
+            }
+            else
+            {
+                lblBarlangNeve.Text = kijBarlang.Nev;
+                tbHosszusag.Text = $"{kijBarlang.Hossz}";
+                tbMelyseg.Text = $"{kijBarlang.Melyseg}";
+            }
         }
 
         private void BarlangokFormLoad(object? sender, EventArgs e)
@@ -25,6 +39,16 @@ namespace BarlangokGUI
             StreamReader sr = new($"{SOLDIR}\\barlangok.txt", Encoding.UTF8);
             _ = sr.ReadLine();
             while (!sr.EndOfStream) barlangok.Add(new(sr.ReadLine()));
+        }
+
+        private void EredetiAllapot()
+        {
+            tbAzonosito.Text = string.Empty;
+            lblBarlangNeve.Text = string.Empty;
+            tbHosszusag.Text = string.Empty;
+            tbMelyseg.Text = string.Empty;
+            btnMentes.Enabled = false;
+            kijBarlang = null;
         }
     }
 }
